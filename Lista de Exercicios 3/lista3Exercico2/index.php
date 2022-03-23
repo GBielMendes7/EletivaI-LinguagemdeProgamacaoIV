881c783526cb3ea68922588617261e8b660be104
GBielMendes7 <99516155+GBielMendes7@users.noreply.github.com>
2022-03-23 20:25:18 -0300

    lista3exercicio2

---
+++ Lista de Exercicios 3/lista3Exercico2/index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Exercicio 2</title>
+  </head>
+  <body class="bg-success bg-gradient">
+
+    <div class="container mt-5 p-4 rounded-3 bg-white shadow w-50">
+      <h1 class="text-center">Sala de aula</h1>
+
+      <form  class="mt-4" method="post" action="resposta.php">
+        <select class="mb-3 form-select bg-light border border-light">
+          <option selected>Selecione a materia</option>
+          <option value="1">Português</option>
+          <option value="2">Matemática</option>
+          <option value="3">Historia</option>
+          <option value="4">Geografia</option>
+        </select>
+
+        <div class="row p-3">
+          <div class="col mb-3">
+            <?php
+                for ($i=1; $i <= 10; $i++){
+            ?>
+            <label for="aluno" class="form-label">Digite o nome do aluno:</label>
+                      
+            <input type="text" class="form-control bg-light border border-light" id="aluno" name="aluno">
+            <?php
+                }
+            ?>
+
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col mb-3">
+            <button type="submit" class="btn btn-success">ENVIAR</button>
+        
+          </div>
+        </div>
+
+      </form>
+
+    </div>
+    
+  </body>
+</html>
---
+++ Lista de Exercicios 3/lista3Exercico2/resposta.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Exercicio 2</title>
+  </head>
+  <body class="mt-5 p-4 bg-success bg-gradient">
+    <h1 class="text-center text-light">Alunos</h1>
+    <?php
+      function ondenar($vetor){
+          sort($vetor);
+        }
+
+      for ($i=1; $i <= 10; $i++){
+        $vetor[$i] = $_POST["aluno"];
+      }
+    
+    ?>
+    <div class="container mt-3 p-4 rounded-3 bg-white shadow w-50 text-center">
+      <?php
+        echo $vetor;
+        ondenar($vetor);
+      ?>
+
+    </div>
+
+
+  </body>
+</html>
