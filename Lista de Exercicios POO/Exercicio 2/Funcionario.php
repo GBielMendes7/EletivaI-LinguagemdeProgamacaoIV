8e24b34b2cf749156526c4872458a810a9021efd
GBielMendes7 <99516155+GBielMendes7@users.noreply.github.com>
2022-05-02 16:50:59 -0300

    exercicio2 parte1

---
+++ Lista de Exercicios POO/Exercicio 2/Funcionario.php
@@ -0,0 +1,56 @@
+<?php
+
+class Funcionario{
+    private $nome;
+    private $codigo;
+    private $salarioBase;
+
+    public function __construct($nome, $codigo, $salarioBase)
+    {
+        $this->setNome($nome);
+        $this->setCodigo($codigo);
+        self::setSalarioBase($salarioBase);
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+    public function setNome($nome){
+        $this->nome = $nome;
+    }
+
+    public function getCodigo(){
+        return $this->codigo;
+    }
+    public function setCodigo($codigo){
+        $this->codigo = $codigo;
+    }
+
+    public function getSalarioBase(){
+        return $this->salarioBase;
+    }
+    public function setSalarioBase($salarioBase){
+        $this->salarioBase = $salarioBase;
+    }
+
+    public function getSalarioLiquido(){
+        $inss = $this->salarioBase * 0.1;
+        $ir = 0.0;
+        if ($this->salarioBase > 2000.00){
+            $ir = ($this->salarioBase-2000.00)*0.12; 
+        }
+        return ($this->salarioBase - $inss - $ir);
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+?>
