48f7544b830a018415e6dd9b207bc28feff78b1b
GBielMendes7 <99516155+GBielMendes7@users.noreply.github.com>
2022-05-04 20:49:33 -0300

    Aula dia 04-05

---
+++ Projeto/bootstrap.php
@@ -0,0 +1,34 @@
+<?php
+
+use EletivaILinguagemdeProgamacaoIv\Projeto\Router;
+
+require_once __DIR__."/vendor/autoload.php";
+
+   //Aqui é recuperado o que foi digitado pelo usuario e qual méthodo HTTP ele utilizou
+
+   $method = $_SERVER["REQUEST_METHOD"];
+   $path = $_SERVER["PATH_INFO"];
+
+   //Instanciar classe Router
+
+   $router = new EletivaILinguagemdeProgamacaoIv\Projeto\Router($method, $path);
+
+   //Adicionar as rotas válidas a baixo
+
+   $router->get("/ola-mundo", function(){
+       return "Olá Mundo!";
+   });
+
+   //Adicionar as rotas válidas a cima
+
+   $result = $router->handler();
+
+   if(!$result){
+       http_response_code(404);
+       echo "Página não encontrada";
+       die();
+   }
+
+   echo $result($router->getParams());
+
+?>
