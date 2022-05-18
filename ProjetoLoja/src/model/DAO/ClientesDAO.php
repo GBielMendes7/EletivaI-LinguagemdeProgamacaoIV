d1877d048926f9c261cfe73fcaccca5eabdc6dc3
GBielMendes7 <99516155+GBielMendes7@users.noreply.github.com>
2022-05-18 20:31:42 -0300

    atividade banco de dados

---
+++ ProjetoLoja/src/model/DAO/ClientesDAO.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Aluno\ProjetoLoja\model\DAO;
+
+use Aluno\ProjetoLoja\model\Entity\Clientes;
+
+class ClienteDAO{
+
+    public function inserir(Clientes $c){
+        try{
+            $sql = "INSERT INTO `clientes`(`nome`, `email`, `idade`) VALUES (:nome, :email, :idade)";
+
+        }catch(\Exception $e){
+            return false;
+        }
+    }
+
+    public function alterar(Clientes $c){
+        try{
+            $sql = "UPDATE `clientes` SET `nome`= :nome,`email`= :email,`idade`= :idade WHERE id = :id";
+
+        }catch(\Exception $e){
+            return false;
+        }
+    }
+
+    public function excluir($id){
+        try{
+            $sql = "DELETE FROM `clientes` WHERE id = :id";
+
+        }catch(\Exception $e){
+            return false;
+        }
+    }
+
+    public function consultar(){
+        try{
+            $sql = "SELECT * FROM clientes";
+
+        }catch(\Exception $e){
+            return false;
+        }
+    }
+
+    public function consultarPorId($id){
+        try{
+            $sql = "SELECT * FROM clientes WHERE id = :id";
+
+        }catch(\Exception $e){
+            return false;
+        }
+    }
+}
+
+
+?>
